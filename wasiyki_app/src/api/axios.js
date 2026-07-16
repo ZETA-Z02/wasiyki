6431e810447fb2f33d859eb218ddc60755b4d928
zeta <jersson.z032@gmail.com>
2026-07-16 16:13:16 -0500

    add

--- wasiyki_app/src/api/axios.js
+++ wasiyki_app/src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+//   baseURL: "http://localhost:8000/api",
+  baseURL: "https://wasiyki-api.zettasky.com/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 // Interceptor para inyectar el token en las peticiones protegidas
