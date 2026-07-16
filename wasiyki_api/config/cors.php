d831b574bcf1b438dc9d919973231c8a5ccbbd80
zeta <jersson.z032@gmail.com>
2026-07-16 18:13:43 -0500

    add

--- wasiyki_api/config/cors.php
+++ wasiyki_api/config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://wasiyki.zettasky.com',
     ],
 
     'allowed_origins_patterns' => [],
